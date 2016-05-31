36cc65a432df4f43accae71700c14898eebb1cb8
SHANKY <sinnisingla@gmail.com>
2016-06-01 01:07:58 +0530

    Customer pojo created

---
+++ src/main/java/customer/Customer.java
@@ -0,0 +1,45 @@
+package customer;
+
+public class Customer {
+	String name;
+	int contact;
+	String address;
+	String city; 
+	String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", contact=" + contact + ", address=" + address + ", city=" + city
+				+ ", email=" + email + "]";
+	}
+	
+}
