37fedfbd24fcedcddb9b443e9eb102be5ff821d0
SHANKY <sinnisingla@gmail.com>
2016-06-04 20:14:14 +0530

    added jax-rs

--- src/main/java/customer/CustomerDao.java
+++ src/main/java/customer/CustomerDao.java
@@ -1,6 +1,5 @@
 package customer;
 
-<<<<<<< HEAD
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,8 +19,3 @@ public class CustomerDao {
 	}
  
 }
-=======
-public class CustomerDao {
-
-}
->>>>>>> 7a0b5ca0adc0b989daed9fce47f23c90d4a1a768
