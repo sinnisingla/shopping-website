f0c49dd3f3d5bb369d66ad9751e905bd4f42137a
ssingl3 <ssingla0@walmartlabs.com>
2016-06-13 14:41:55 +0530

    jersey configured 3

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
  
 @Path("/hello")
 public class CustomerDao {
